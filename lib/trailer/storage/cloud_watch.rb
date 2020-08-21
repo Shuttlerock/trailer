fb73b17599130afb9350689aac281356e683ff5c
Dave Perrett <dave@recurser.com>
2020-08-22 00:07:36 +0900

    [ch68592] Handle expired sequence_token.

--- lib/trailer/storage/cloud_watch.rb
+++ lib/trailer/storage/cloud_watch.rb
@@ -10,7 +10,9 @@ module Trailer::Storage
     # Constructor.
     def initialize
       @messages = []
-      connect!
+      @client   = Aws::CloudWatchLogs::Client.new(region: Trailer.config.aws_region, credentials: credentials)
+      ensure_log_group
+      ensure_log_stream
     end
 
     # Queues the given hash for writing to CloudWatch.
@@ -39,15 +41,25 @@ module Trailer::Storage
       response        = client.put_log_events(events)
       @sequence_token = response&.next_sequence_token
       @messages       = []
+    rescue Aws::CloudWatchLogs::Errors::InvalidSequenceTokenException
+      # Only one client at a time can write to the log. If another client has written before we get a chance,
+      # the sequence token is invalidated, and we need to get a new one.
+      @sequence_token = log_stream[:upload_sequence_token]
+      retry
     end
 
     private
 
     attr_accessor :client, :messages, :sequence_token
 
-    # Create the log group, if it doesn't already exist.
-    # Ideally we would paginate here in case the account has a lot of log groups.
-    def create_log_group
+    # Returns an AWS credentials instance for writing to CloudWatch.
+    def credentials
+      Aws::Credentials.new(Trailer.config.aws_access_key_id, Trailer.config.aws_secret_access_key)
+    end
+
+    # Creates the log group, if it doesn't already exist. Ideally we would paginate here in case
+    # the account has a lot of log groups with the same prefix, but it seems unlikely to happen.
+    def ensure_log_group
       existing = client.describe_log_groups.log_groups.find do |group|
         group.log_group_name == Trailer.config.application_name
       end
@@ -59,33 +71,27 @@ module Trailer::Storage
 
     # Create the log stream, if it doesn't already exist.
     # Ideally we would paginate here in case the account has a lot of log streams.
-    def create_log_stream
-      existing = client.describe_log_streams(log_group_name: Trailer.config.application_name).log_streams.find do |stream|
-        stream.log_stream_name == Trailer.config.application_name
+    def ensure_log_stream
+      if (existing = log_stream)
+        @sequence_token = existing.upload_sequence_token
+      else
+        client.create_log_stream(
+          log_group_name:  Trailer.config.application_name,
+          log_stream_name: Trailer.config.application_name,
+        )
       end
-
-      puts existing
-
-      existing ||= client.create_log_stream(
-        log_group_name:  Trailer.config.application_name,
-        log_stream_name: Trailer.config.application_name,
-      )
-
-      @sequence_token = existing.upload_sequence_token
     rescue Aws::CloudWatchLogs::Errors::ResourceAlreadyExistsException
       # No need to do anything - probably caused by lack of pagination.
     end
 
-    # Instantiates a CloudWatch client, and makes sure we have a group and stream to log to.
-    def connect!
-      @client = Aws::CloudWatchLogs::Client.new(region: Trailer.config.aws_region, credentials: credentials)
-      create_log_group
-      create_log_stream
-    end
-
-    # Returns AWS credentials for writing to CloudWatch.
-    def credentials
-      Aws::Credentials.new(Trailer.config.aws_access_key_id, Trailer.config.aws_secret_access_key)
+    # Returns the current log stream, if one exists.
+    def log_stream
+      client.describe_log_streams(
+        log_group_name:         Trailer.config.application_name,
+        log_stream_name_prefix: Trailer.config.application_name,
+      ).log_streams.find do |stream|
+        stream.log_stream_name == Trailer.config.application_name
+      end
     end
   end
 end
